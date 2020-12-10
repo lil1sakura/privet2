8c1bca0fea50c2579fae8574e690e73a88356fd6
Tim <timproskur@gmail.com>
2020-12-10 23:45:33 +1000

    privet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt5 import uic
 from PyQt5.QtCore import QPointF
 from PyQt5.QtGui import QPainter, QColor
@@ -9,9 +9,12 @@ import random
 class MainForm(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('untitled.ui', self)
+        self.setGeometry(700, 500, 350, 402)
         self.c = False
-        self.pushButton.clicked.connect(self.paint)
+        self.b = QPushButton('кнопка', self)
+        self.b.resize(100, 50)
+        self.b.move(120, 30)
+        self.b.clicked.connect(self.paint)
 
     def paint(self):
         self.c = True
@@ -25,10 +28,9 @@ class MainForm(QMainWindow):
             qp.end()
 
     def draw(self, qp):
-        num = random.randint(1, 150)
-        qp.setBrush(QColor(255, 255, 0))
-        qp.drawEllipse(QPointF(250, 150), num, num)
-
+        num = random.randint(1, 130)
+        qp.setBrush(QColor(random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)))
+        qp.drawEllipse(QPointF(170, 250), num, num)
 
 
 if __name__ == '__main__':
